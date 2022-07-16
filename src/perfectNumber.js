4d2413f0cf6c85aadae8fa1f45f61e2163e1b696
BatuhanB <balibatuhan19@gmail.com>
2022-07-16 13:45:44 +0300

    third assignment is done(findPerfectNumbers(optional))

--- src/perfectNumber.js
+++ src/perfectNumber.js
@@ -1,18 +1,35 @@
 let findPerfectNumber = (lastIndex) => {
-    total = 0;
+  total = 0;
   for (let i = 0; i < lastIndex; i++) {
-    if(lastIndex % i == 0){
-        total += i;
+    if (lastIndex % i == 0) {
+      total += i;
     }
-    
-  }  
-  if(total == lastIndex){
-    console.log(lastIndex + " is perfect number!")
-    }  else{
-        console.log(lastIndex + " is not perfect number")
-    }
+  }
+  if (total == lastIndex) {
+    console.log(lastIndex + " is perfect number!");
+  } else {
+    console.log(lastIndex + " is not perfect number");
+  }
 };
-findPerfectNumber(6);
-findPerfectNumber(28);
-findPerfectNumber(468);
-findPerfectNumber(496);
+// findPerfectNumber(6);
+// findPerfectNumber(28);
+// findPerfectNumber(468);
+// findPerfectNumber(496);
+
+let findPerfectNumbers = (lastIndex) => {
+  let perfectNumbers = [];
+  total = 0;
+  for (let i = 1; i < lastIndex; i++) {
+    for (let j = 1; j <= i - 1; j++) {
+      if (i % j == 0) {
+        total += j;
+      }
+    }
+    if (i == total) {
+      perfectNumbers.push(i);
+    }
+    total = 0;
+  }
+  console.log("perfect numbers: " + perfectNumbers);
+};
+findPerfectNumbers(1000);
